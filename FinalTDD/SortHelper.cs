00ef8eeaf6210690ccc66bcb6b59e13d1fc51f66
RayanMT <157949161+RayanMT@users.noreply.github.com>
2025-05-17 19:41:52 +0300

    Update: fixed [added the old ver of the code]

--- FinalTDD/SortHelper.cs
+++ FinalTDD/SortHelper.cs
@@ -53,3 +53,34 @@ namespace FinalTDD
     }
 
 }
+
+
+// ==============================
+// Old Version (Required by Prof)
+// ==============================
+
+/*
+public static class SortHelper
+{
+    public static List<Vehicle> BubbleSort(List<Vehicle> list)
+    {
+        var sortedList = new List<Vehicle>(list);
+        int n = sortedList.Count;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (sortedList[j].Year < sortedList[j + 1].Year)
+                {
+                    var temp = sortedList[j];
+                    sortedList[j] = sortedList[j + 1];
+                    sortedList[j + 1] = temp;
+                }
+            }
+        }
+
+        return sortedList;
+    }
+}
+*/
